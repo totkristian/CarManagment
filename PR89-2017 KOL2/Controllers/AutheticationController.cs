151a2f6e3796eebabb7d19c9a35414e9ddc66798
Kristian Tot <kristian.tot@pbssolutions.net>
2020-05-26 21:20:28 +0200

    added registration view and controller also edited the navbar

--- PR89-2017 KOL2/Controllers/AutheticationController.cs
+++ PR89-2017 KOL2/Controllers/AutheticationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PR89_2017_KOL2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,27 @@ namespace PR89_2017_KOL2.Controllers
         // GET: Authetication
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Korisnik korisnik)
+        {
+            Dictionary<string, Korisnik> korisnici = (Dictionary<string, Korisnik>)HttpContext.Application["korisnici"];
+           // Korisnik k = korisnici.Where(x => x.Value.Equals(korisnik)).Select(x => x.Value).Single();
+            if(korisnici.ContainsValue(korisnik) || korisnici.ContainsKey(korisnik.KorisnickoIme))
+            {
+                ViewBag.Message = $"Korisnik sa korisnickim imenom {korisnik.KorisnickoIme} vec postoji!";
+                return View();
+            }
+            korisnici.Add(korisnik.KorisnickoIme, korisnik);
+            Session["korisnik"] = korisnik;
+            ViewBag.Message = $"Uspesno ste se registrovali!";
             return View();
         }
     }
--- PR89-2017 KOL2/Models/Korisnik.cs
+++ PR89-2017 KOL2/Models/Korisnik.cs
@@ -11,8 +11,7 @@ namespace PR89_2017_KOL2.Models
         
         private int id;
 
-        [Required(ErrorMessage ="Korisnicko ime je obavezno!")]
-        [StringLength(int.MaxValue, MinimumLength = 3)]
+        [Required(ErrorMessage ="Korisnicko ime je obavezno!"),MinLength(3)]
         private string korisnickoIme;
         [Required(ErrorMessage = "Lozinka je obavezna!")]
         [RegularExpression(@"[a-zA-Z0-9]{8,}",ErrorMessage ="Mora biti minimum 8 karaktera!")]
@@ -38,6 +37,53 @@ namespace PR89_2017_KOL2.Models
         public string Email { get => email; set => email = value; }
         public DateTime DatumRodjenja { get => datumRodjenja; set => datumRodjenja = value; }
         public Role Uloga { get => uloga; set => uloga = value; }
-        
+
+        public override bool Equals(object obj)
+        {
+            var korisnik = obj as Korisnik;
+            return korisnik != null &&
+                   id == korisnik.id &&
+                   korisnickoIme == korisnik.korisnickoIme &&
+                   lozinka == korisnik.lozinka &&
+                   ime == korisnik.ime &&
+                   prezime == korisnik.prezime &&
+                   pol == korisnik.pol &&
+                   email == korisnik.email &&
+                   datumRodjenja == korisnik.datumRodjenja &&
+                   uloga == korisnik.uloga &&
+                   Id == korisnik.Id &&
+                   KorisnickoIme == korisnik.KorisnickoIme &&
+                   Lozinka == korisnik.Lozinka &&
+                   Ime == korisnik.Ime &&
+                   Prezime == korisnik.Prezime &&
+                   Pol == korisnik.Pol &&
+                   Email == korisnik.Email &&
+                   DatumRodjenja == korisnik.DatumRodjenja &&
+                   Uloga == korisnik.Uloga;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1055405030;
+            hashCode = hashCode * -1521134295 + id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(korisnickoIme);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(lozinka);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ime);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(prezime);
+            hashCode = hashCode * -1521134295 + pol.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(email);
+            hashCode = hashCode * -1521134295 + datumRodjenja.GetHashCode();
+            hashCode = hashCode * -1521134295 + uloga.GetHashCode();
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(KorisnickoIme);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Lozinka);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Ime);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Prezime);
+            hashCode = hashCode * -1521134295 + Pol.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Email);
+            hashCode = hashCode * -1521134295 + DatumRodjenja.GetHashCode();
+            hashCode = hashCode * -1521134295 + Uloga.GetHashCode();
+            return hashCode;
+        }
     }
 }
